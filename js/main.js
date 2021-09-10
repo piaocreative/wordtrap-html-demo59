5a62e2f049729ecbe463f5a4cabf77436c9570c1
piaocreative <johnpiao171019@gmail.com>
2021-09-11 04:09:10 +0800

    demo59-0911

--- js/main.js
+++ js/main.js
@@ -1,101 +1,22 @@
 $( document ).ready( function() {
 
-  $('.logos').slick({
-    dots: false,
-    arrows: true,
+  $('.banners').slick({
+    dots: true,
+    infinite: true,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-});
-
-  $('.posts').slick({
-      dots: false,
-      arrows: true,
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows:false,
   });
 
-  $('.products').slick({
-    dots: false,
-    arrows: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+  $(".banner-prev").click(function(e) {
+    e.preventDefault();
+    $('.banners').slick('slickPrev');
+  })
+  
+  $(".banner-next").click(function(e) {
+    e.preventDefault();
+    $('.banners').slick('slickNext');
+  })
 
 });
